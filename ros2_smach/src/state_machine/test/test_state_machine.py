a1cdbb602e28abafafccb6b6617835fc6faea0ec
salvin.george <salvin.george@fkie.fraunhofer.de>
2024-07-01 17:17:57 +0200

    update: unittest implementation (working)

---
+++ ros2_smach/src/state_machine/test/test_state_machine.py
@@ -0,0 +1,45 @@
+import unittest
+import rclpy
+
+from rclpy.node import Node
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+from smach import StateMachine
+
+from state_machine.state_machine import MonitorBatteryAndCollision, Move, RotateBase, StopMotion, main
+
+'''refered from https://answers.ros.org/question/356180/ros2-creating-integration-tests-for-python-nodes/'''
+
+class TestSmachStateMachine(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+        cls.test_node = rclpy.create_node('test_node')
+        cls.test_node.get_logger().info('Node created')
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_node.get_logger().info('Destroying node')
+        cls.test_node.destroy_node()
+        rclpy.shutdown()
+    
+    def test_battery_callback(self):
+        self.test_node.get_logger().info('Testing battery callback')
+        state = MonitorBatteryAndCollision(self.test_node)
+        msg = Float32()
+        msg.data = 25.0
+        state.battery_callback(msg)
+        self.assertEqual(state.battery_level, 25.0)
+
+    def test_laser_callback(self):
+        self.test_node.get_logger().info('Testing laser callback')
+        state = MonitorBatteryAndCollision(self.test_node)
+        msg = LaserScan()
+        msg.ranges = [1.0, 2.0, 3.0, 4.0, 5.0]
+        state.collision_callback(msg)
+        self.assertEqual(state.collision_distance, 1.0)
+
+    
+if __name__ == '__main__':
+    unittest.main()
